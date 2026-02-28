a70a6db9c58644c2b94e2346b5cb79a398a3eff7
thaleszm <marquesthales345@gmail.com>
2026-02-28 15:26:49 -0300

    fix: solve page problem

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -22,11 +22,11 @@ export default function ParticlesBackground() {
       }
 
       draw() {
-        ctx.beginPatch();
+        ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.shadowBlur = 10;
         ctx.shadowColor = this.color;
-        ctx.fillStyle - this.color;
+        ctx.fillStyle = this.color;
         ctx.fill();
       }
       update() {
