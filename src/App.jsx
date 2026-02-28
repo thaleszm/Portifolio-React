5edaecc7d3047dab581524457e4abd833d3ffaf5
thaleszm <marquesthales345@gmail.com>
2026-02-28 13:36:47 -0300

    chore: to update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-import { useState } from "react";
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
+import Footer from "./sections/Footer";
+import Home from "./sections/Home";
+import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
+import Testimonials from "./sections/Testimonials";
+import Navbar from "./components/Navbar";
 
-function App() {}
-
-export default App;
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
